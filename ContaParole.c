7b3383fb78405e1c64898b10c687f250cbdc297d
Giandomenico-bit <giandomenicoiameo00@gmail.com>
2021-02-20 01:38:15 +0100

    Aggiunto algoritmo Contaparole

---
+++ ContaParole.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <ctype.h>
+
+int main( void ) {
+  
+  char ch;
+  
+  unsigned int parole = 0;
+  unsigned int caratteri = 0;
+  unsigned int riga = 0;
+  
+  FILE* fpunt = fopen( "file.txt", "r" );
+  if( fpunt == NULL ) {
+	  fprintf( stderr, "Errore!" );
+	  return 1;
+   }
+   
+   while( ( ch = fgetc( fpunt ) ) != EOF ) {
+	   if( isblank( ch ) ) 
+		   parole++;
+	   else if( ch == '\n' )
+		   riga++;
+	   else
+		   caratteri++;
+   }
+   
+   printf( "Parole totali : %u\n", parole + 1 );
+   printf( "Righe torali : %u\n", riga );
+   printf( "Caratteri totali : %u\n", caratteri - 1 );
+
+  return 0;
+}
